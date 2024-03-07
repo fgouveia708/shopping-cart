d1031e43bc7aa1476517251a7a996a84f0b4b42f
Fernando Gouveia <fgouveia.dev@outlook.com>
2024-03-07 16:28:48 -0300

    feat: create app component and module

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, RouterModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
