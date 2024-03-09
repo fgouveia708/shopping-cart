770f59b633ca52c69b6b6d10a1de552545a34c58
Fernando Gouveia <fgouveia.dev@outlook.com>
2024-03-09 12:49:58 -0300

    refact: refactor code

--- src/core/services/product/product.service.ts
+++ src/core/services/product/product.service.ts
@@ -1,15 +1,13 @@
-
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProductResponse } from './dtos/product.response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getProducts(): Observable<ProductResponse[]> {
     return this.http.get<ProductResponse[]>('https://raw.githubusercontent.com/eduardovianna/digi/main/products.json');
